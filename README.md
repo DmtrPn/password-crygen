c3607dad907792d21a60c05876ed4e589aadd344
Dima Panov <rpsdpano@gmail.com>
2022-03-08 18:22:27 +0300

    some fix + add readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # password-crypto
-шифратор/чекер паролей
+
+
+#### Generate password hash
+
+```typescript
+import { generatePasswordHash } from 'password-crypto';
+
+const password = '123qwe';
+const passwordHash = generatePasswordHash(password);
+```
+
+#### Check password
+
+```typescript
+import { checkPassword } from 'password-crypto';
+
+const passwordToChech = '123qwe';
+const passwordHash = 'hex...';
+const isSame = checkPassword(passwordToChech, passwordHash);
+```
+
+
--- src/CryptoGenerater.ts
+++ src/CryptoGenerater.ts
@@ -5,26 +5,28 @@ const KEYLEN = 100;
 const DIGSET = 'sha512';
 const SALT_SIZE = 16;
 
-export class CryptoGenerater {
+export function generatePasswordHash(pass: string, salt?: string): string {
+    const passSalt = salt || generateSalt();
 
-    public static generatePasswordHash(pass: string, salt?: string): string {
-        const passSalt = salt || CryptoGenerater.generateSalt();
-
-        const isPasswordNotTrimmed = pass !== pass.trim();
-        if (isPasswordNotTrimmed) {
-            throw new Error('Password can not include spaces at the beginning and the end.');
-        }
-        const hash =
-            crypto.pbkdf2Sync(pass, passSalt, ITERATION_COUNT, KEYLEN, DIGSET);
-        return passSalt + hash.toString('hex');
+    const isPasswordNotTrimmed = pass !== pass.trim();
+    if (isPasswordNotTrimmed) {
+        throw new Error('Password can not include spaces at the beginning and the end.');
     }
+    const hash = crypto.pbkdf2Sync(pass, passSalt, ITERATION_COUNT, KEYLEN, DIGSET);
+    return passSalt + hash.toString('hex');
+}
 
-    public static generateSalt(): string {
-        const salt = crypto.randomBytes(SALT_SIZE).toString('hex');
-        return salt.substr(0, SALT_SIZE);
-    }
+export function checkPassword(passwordString: string, passwordHash: string): boolean {
+    const salt = getSalt(passwordHash);
+    const password = generatePasswordHash(passwordString, salt);
+    return password === passwordHash;
+}
 
-    public static getSalt(pass: string): string {
-        return pass.substr(0, SALT_SIZE);
-    }
+export function generateSalt(): string {
+    const salt = crypto.randomBytes(SALT_SIZE).toString('hex');
+    return salt.substr(0, SALT_SIZE);
+}
+
+export function getSalt(pass: string): string {
+    return pass.substr(0, SALT_SIZE);
 }
